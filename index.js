5bdf05c54483db2892f4da9c901f5f185e26451f
Aniket2805 <aniketrocks2468@gmail.com>
2024-04-30 13:27:19 +0530

    w

---
+++ index.js
@@ -0,0 +1,8 @@
+//External JS: https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js, see settings
+
+const lightbox = GLightbox({
+  touchNavigation: true,
+  loop: true,
+  width: "90vw",
+  height: "90vh",
+});
